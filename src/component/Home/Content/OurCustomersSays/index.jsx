f3c9499ac0abf4d995382ecac5551a8d7e6a787d
hoangeel <lvhoangbagia@gmail.com>
2022-09-23 08:37:27 +0700

    first commit

--- src/component/Home/Content/OurCustomersSays/index.jsx
+++ src/component/Home/Content/OurCustomersSays/index.jsx
@@ -71,15 +71,15 @@ export default function OurCustomersSays() {
         </div>
         <div className='slider-2'>
           <div className='flexOurCustomers'>
-            <div className='leftOurCustomers'>
+            <div className='leftOurCustomers rightOurCustomers' >
               <Card className='flexleftOurCustomers'>
-                <Card.Title className='CardTitleOur'>Provided invaluable expertise and support for our digital and marketing technology projects </Card.Title>
+                <Card.Title className='CardTitleOur'>240,000 centre visitors enrolled in loyalty, 15% of annual unique visitors, and 50% above goal </Card.Title>
                 <Card.Text className='CardTextOur'>I found Simplaq to be a flexible and innovative multi-platform tool that assists me in my day to day task as a centre manager. The ability of having it work seamlessly on my desktop as well as my mobile device is invaluable and allows me a great flexibility and a single point of data retrieval no matter where I am (my desk or a remote meeting). </Card.Text>
                 <div className='footerOurCustomers'>
                   <div className='imgMess'><img src={Mess} alt="" /></div>
                   <div className='groupOurCustomersSays'>
                     <div className='name'>Nolan Carder</div>
-                    <div className='position'>Shopping Center Manager</div>
+                    <div className='position'>Regional director marketing</div>
                   </div>
                   <div className='groupStar ms-auto'>
                     <div className='star'><img src={Icon} alt="" /></div>
@@ -91,15 +91,15 @@ export default function OurCustomersSays() {
                 </div>
               </Card>
             </div>
-            <div className='leftOurCustomers rightOurCustomers' >
+            <div className='leftOurCustomers marginLeftOur'>
               <Card className='flexleftOurCustomers'>
-                <Card.Title className='CardTitleOur'>240,000 centre visitors enrolled in loyalty, 15% of annual unique visitors, and 50% above goal </Card.Title>
+                <Card.Title className='CardTitleOur'>Provided invaluable expertise and support for our digital and marketing technology projects </Card.Title>
                 <Card.Text className='CardTextOur'>I found Simplaq to be a flexible and innovative multi-platform tool that assists me in my day to day task as a centre manager. The ability of having it work seamlessly on my desktop as well as my mobile device is invaluable and allows me a great flexibility and a single point of data retrieval no matter where I am (my desk or a remote meeting). </Card.Text>
                 <div className='footerOurCustomers'>
                   <div className='imgMess'><img src={Mess} alt="" /></div>
                   <div className='groupOurCustomersSays'>
                     <div className='name'>Nolan Carder</div>
-                    <div className='position'>Regional director marketing</div>
+                    <div className='position'>Shopping Center Manager</div>
                   </div>
                   <div className='groupStar ms-auto'>
                     <div className='star'><img src={Icon} alt="" /></div>
